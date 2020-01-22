1a95bd1fab651827ebbb97eda4269b0466ce3fc1
wilson-song-git <wilsonsong0000@gmail.com>
2020-01-22 12:14:12 -0600

    created magic 8 ball app, pushed to git

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -4,15 +4,24 @@
 //
 //  Created by Angela Yu on 14/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
+
+//Wilson Song
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ballView: UIImageView!
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let randChoice = Int.random(in: 0...4)
+        
+        ballView.image = ballArray[randChoice]
+    }
+    
 
 }
 
